cb87c2af2d1c850eeb8c74c7e1009e034bdce1b9
ikeacat <mfryk268@gmail.com>
2021-06-09 15:09:23 -0500

    HTTP to NS & Validation of credentials

--- lib/DoingInstallWidget.dart
+++ lib/DoingInstallWidget.dart
@@ -6,6 +6,7 @@
 // Last Updated 9 June 2021
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class DoingInstallWidget extends StatefulWidget {
   DoingInstallWidget({Key? key, this.platform, this.username, this.password})
@@ -21,16 +22,86 @@ class DoingInstallWidget extends StatefulWidget {
 class DIState extends State<DoingInstallWidget> {
   var currentMessage = '';
 
-  void doInstall() {
+  void doInstall(BuildContext context) async {
     // Ping NS to verify credentials.
     setState(() {
       currentMessage = 'Verifying credentials with NS...';
     });
+    var ping = await pingNS();
+    print(ping.headers);
+    //print(ping.body);
+    print(ping.statusCode);
+    if (ping.statusCode == 403) {
+      errorAlertAndPop(
+          "The password provided for this nation is invalid. (Status Code 403 Forbidden)",
+          context);
+      return;
+    } else if (ping.statusCode == 409) {
+      errorAlertAndPop(
+          'There was a conflict in the request. (Status Code 409 Conflict)',
+          context);
+      return;
+    } else if (ping.statusCode == 418) {
+      errorAlertAndPop(
+          'NationStates is a teapot. (Status Code 418 Im a Teapot.)', context);
+      return;
+    } else if (ping.statusCode == 429) {
+      errorAlertAndPop(
+          'There were too many requests to the NationStates API from this device. Try again in ${ping.headers["X-Retry-After"]} seconds. (Status Code 429 Too Many Requests)',
+          context);
+      return;
+    } else {
+      errorAlertAndPop(
+          "NationStates didn't respond with a 200 status code. (HTTP Status Code ${ping.statusCode})",
+          context);
+      return;
+    }
+  }
+
+  void errorAlertAndPop(String message, BuildContext context) {
+    AlertDialog alert = AlertDialog(
+      title: Text(
+        'Error',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      content: Text(message),
+      actions: [
+        TextButton(
+            onPressed: () {
+              // Pop to the form
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'Ok',
+              style: TextStyle(color: Colors.purple),
+            ))
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (context) {
+          return alert;
+        });
+  }
+
+  Future<Response> pingNS() async {
+    final Response response = await post(
+        Uri.parse('https://www.nationstates.net/cgi-bin/api.cgi'),
+        headers: {
+          'User-Agent': 'NoPerish <mfryk268@gmail.com>',
+          'X-Password': widget.password!
+        },
+        body: {
+          'nation': widget.username!,
+          'q': 'ping'
+        });
+    return response;
   }
 
   @override
   Widget build(BuildContext context) {
-    doInstall();
+    doInstall(context);
     return Scaffold(
       body: Center(
         child: Column(
