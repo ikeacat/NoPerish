d12041cddf1773a12de69cb822c16e94fa2d1fae
ikeacat <mfryk268@gmail.com>
2021-06-28 12:55:49 -0500

    Lol dates

--- lib/misc/ChangelogWidget.dart
+++ lib/misc/ChangelogWidget.dart
@@ -3,7 +3,7 @@
 // Public License v3.0.
 // Get a copy here: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 // Or just look at the LICENSE file.
-// Last Updated 24 June 2021
+// Last Updated 28 June 2021
 
 import 'package:flutter/material.dart';
 import 'package:noperish/misc/BoldTextBar.dart';
