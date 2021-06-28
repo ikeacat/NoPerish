1c00850b3103ff28f0ead866459d01ebf5ae267f
ikeacat <mfryk268@gmail.com>
2021-06-28 14:06:32 -0500

    bussin' bussin'

--- lib/misc/VersionConstant.dart
+++ lib/misc/VersionConstant.dart
@@ -3,6 +3,6 @@
 // Public License v3.0.
 // Get a copy here: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 // Or just look at the LICENSE file.
-// Last Updated 24 June 2021
+// Last Updated 28 June 2021
 
 const versionNoPerish = '1.2.1';
